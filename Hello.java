5577c9ce262eec7952b2674941ac7cba49ece95d
vivek1 <bhukyavivek007@gmail.com>
2024-12-21 12:34:00 +0530

    program to print large number

--- Hello.java
+++ Hello.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 numbers :");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        if(num1>num2){
+            System.out.println("num1 is max");
+        }else{
+            System.out.println("num2 is max");
+        }
     }
 }
